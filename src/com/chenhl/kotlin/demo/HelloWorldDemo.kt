b45ae69366b71971fa517faa153668eff77fb076
chenh <chenhonglei@outlook.com>
2018-06-11 21:35:18 +0800

    add kotlin hello world

--- src/com/chenhl/kotlin/demo/HelloWorldDemo.kt
+++ src/com/chenhl/kotlin/demo/HelloWorldDemo.kt
@@ -1,2 +1,21 @@
 package com.chenhl.kotlin.demo
 
+import java.util.function.Consumer
+
+fun main(args: Array<String>) {
+//    println("hello world")
+
+    val list: List<String> = listOf("hello", "world", "hello world")
+
+    for (str in list) {
+        println(str)
+    }
+
+    println("-------")
+
+    list.forEach(Consumer { println(it) }) // it表示当前遍历的那个元素
+
+    println("-------")
+
+    list.forEach(System.out::println)
+}
